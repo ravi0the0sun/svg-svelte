e55c7ab0a1bcb45054708f597287d4d967eb2d95
ravi <punravi73@gamil.com>
2020-06-16 20:49:47 +1000

    random svg pop up

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,41 @@
 <script>
-	import Router from 'svelte-spa-router';
-	import { onMount } from 'svelte';
+	import {onMount} from 'svelte';
+	import { fade, draw } from 'svelte/transition';
+	import { butterfly } from './svg/butterfly';
+	import { crane } from './svg/crane';
+	import { cup } from './svg/cup'
 
-	import routes from './routes'
-
-	
-	
+	let duration = 3000;
+	let data;
+	let view = false;
+	function clickHandler() {
+		view? view = false: view = true;
+		let random = Math.floor(Math.random() * Math.floor(3));
+		if (random === 0) {
+			data = crane;
+		} else if (random === 1) {
+			data = butterfly;
+		} else {
+			data = cup;
+		};
+		setTimeout(() => {
+		document.querySelectorAll('path').forEach(path => {
+				path.style.fill = 'black';
+		})
+	}, 2000);
+	}
 </script>
 
-<body>
-	<nav>
-		<ul>
-			<li><a href="#/">Crane</a></li>
-			<li><a href="#/cup">Cup</a></li>
-			<li><a href="#/butterfly">Butterfly</a></li>
-		</ul>
-	</nav>
-	<Router {routes}/>
-	
-</body>
+<button on:click={clickHandler}>click me</button>
+{#if view}
+<div class="crane">
+	<svg
+	sodipodi:docname="vector-butterfly.svg"
+	viewBox="0 0 460 460">  
+	<g out:fade="{{duration: 200}}">
+		<path in:draw="{{duration}}"
+		d={data}/>
+	</g>
+	</svg>
+</div>
+{/if}
